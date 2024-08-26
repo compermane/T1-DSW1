f544b493c6c136ca1623783a54d524803394846c
compermane <comper.mane@gmail.com>
2024-08-26 14:16:58 -0300

    t3 init

---
+++ T3_DSW1/src/main/java/br/ufscar/dc/dsw/T3_DSW1/T3Dsw1Application.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.T3_DSW1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3Dsw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3Dsw1Application.class, args);
+	}
+
+}
---
+++ T3_DSW1/src/test/java/br/ufscar/dc/dsw/T3_DSW1/T3Dsw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.T3_DSW1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3Dsw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
